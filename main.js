055810b5f977709f2d8b7e8cdecc07e2be0f28a9
Renan <reenan.martinho@gmail.com>
2021-02-16 08:45:12 -0300

    Alterações

---
+++ main.js
@@ -0,0 +1,16 @@
+var nome = "Renan Martinho";
+var idade = 24;
+var idade2 = 25;
+alert ("meu pri");
+var frase = "japao é o melhor time do mundo ";
+
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("japao", "corinthians"));
+
+
+//alert(frase.replace)
+
+console.log(frase.toUpperCase());  //informa no console
+
+console.log(frase.toLowerCase());
